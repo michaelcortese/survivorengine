4ba783cde5ea108ce6de0f21828ba3c97aaea93c
Michael Cortese <mcortese2@wisc.edu>
2025-08-27 14:18:14 -0500

    add 6 players

--- src/commands/game/start.ts
+++ src/commands/game/start.ts
@@ -23,6 +23,24 @@ export default {
         .setName("player3")
         .setDescription("player three")
         .setRequired(true);
+    })
+    .addUserOption((option) => {
+      return option
+        .setName("player4")
+        .setDescription("player four")
+        .setRequired(false);
+    })
+    .addUserOption((option) => {
+      return option
+        .setName("player5")
+        .setDescription("player five")
+        .setRequired(false);
+    })
+    .addUserOption((option) => {
+      return option
+        .setName("player6")
+        .setDescription("player six")
+        .setRequired(false);
     }),
   async execute(interaction: ChatInputCommandInteraction) {
     if (Game.active) {
@@ -34,10 +52,23 @@ export default {
       interaction.options.getUser("player2", true),
       interaction.options.getUser("player3", true),
     ];
+    // Add up to 3 more optional players
+    for (let i = 4; i <= 6; i++) {
+      const user = interaction.options.getUser(`player${i}`);
+      if (user) {
+        users.push(user);
+      }
+    }
 
     const players: Player[] = [];
     for (const user of users) {
       players.push(new Player(user.id, user.displayName));
+    }
+
+    // Shuffle the players array for random turn order
+    for (let i = players.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [players[i], players[j]] = [players[j], players[i]];
     }
 
     Game.startGame(players);
