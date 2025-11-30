eb275fce844edb08a3564724a79f5e95dbd90ce5
Michael Cortese <mcortese2@wisc.edu>
2025-11-29 20:52:04 -0600

    finish inheritance functionality

--- src/commands/game/inheritance.ts
+++ src/commands/game/inheritance.ts
@@ -10,4 +10,77 @@ const REQUIRED_CARD = null;
 const INTERRUPTABLE = false;
 const STOPPING_INTERACTION = false;
 const CAN_BE_PLAYED_TRIBAL_COUNCIL = true;
-const ONLY_DURING_TRIBAL_COUNCIL = true;
+const ONLY_DURING_TRIBAL_COUNCIL = false;
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("inheritance")
+    .setDescription("Play inheritance to inherit an eliminated player's cards")
+    .addUserOption((option) =>
+      option
+        .setName("player")
+        .setDescription("The player to inherit from")
+        .setRequired(true),
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+
+    const result = Game.checkForError(
+      interaction,
+      HAS_TARGET,
+      REQUIRED_CARD,
+      INTERRUPTABLE,
+      STOPPING_INTERACTION,
+      CAN_BE_PLAYED_TRIBAL_COUNCIL,
+      ONLY_DURING_TRIBAL_COUNCIL,
+    );
+    if ("error" in result) {
+      return interaction.reply({
+        content: result.error.content,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+    const { player, targetPlayer } = result;
+    if (!targetPlayer) {
+      return interaction.reply({
+        content: "Target player not found.",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    // check for card -- again??
+
+    let inheritance_name = `Inheritance: ${targetPlayer.username}`;
+    if (!player.hasCard(inheritance_name)) {
+       return interaction.reply({
+        content: "You dont have the card",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    // check if target player has 0 lives left
+
+    if (targetPlayer.isAlive()) {
+      return interaction.reply({
+        content: "the player is still alive",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+    
+
+    // checks passed, perform hand transfer logic
+    let cardNames = [];
+    targetPlayer.hand.forEach((card) => {
+      player.hand.push(card);
+      targetPlayer.removeCard(card.name);
+      cardNames.push(card.name);
+    });
+    await interaction.reply({
+      content: `You received ${cardNames}`,
+      flags: MessageFlags.Ephemeral,
+    });
+
+    return interaction.reply({
+      content: `${player.name} inherited ${cardNames.length} from ${targetPlayer.name}`
+    });
+  }
+}
