e1e7d050fe2dbcec97913c899893a4aaca7845e9
Michael Cortese <mcortese2@wisc.edu>
2025-12-18 12:17:13 -0600

    fix lint

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,39 +1,39 @@
-import { Events, MessageFlags, ChatInputCommandInteraction, Client, Collection } from 'discord.js';
+import { Events, MessageFlags, ChatInputCommandInteraction, Collection } from 'discord.js';
 
 interface Command {
-    data: any;
-    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  data: any;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
 
 declare module 'discord.js' {
-    interface Client {
-        commands: Collection<string, Command>;
-    }
+  interface Client {
+    commands: Collection<string, Command>;
+  }
 }
 
 export default {
-    name: Events.InteractionCreate,
-    async execute(interaction: ChatInputCommandInteraction) {
-        if (!interaction.isChatInputCommand()) return;
+  name: Events.InteractionCreate,
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
 
-        try {
-            await command.execute(interaction);
-        }
-        catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-            }
-            else {
-                await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-            }
-        }
-    },
+    try {
+      await command.execute(interaction);
+    }
+    catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+      }
+      else {
+        await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+      }
+    }
+  },
 };
